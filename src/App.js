d5fee3061878bdc1a403573e2dd6fdf2c54153ab
Muhammad Owais <malikowaisjavaid@gmail.com>
2021-11-25 15:21:38 +0500

    api changed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Map from "./Component/Map/Map";
 import "./App.css";
 
 function App() {
-    const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key="AIzaSyDSMMLhjDrtndT6VAnnaJ6NdjxcJlevDLw"`;
+    const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDCNkvhRgNcVWzX26GgylPyIJFB8bGxRj0`;
     return (
         <div className="App">
             GOOGLE MAPS ADDED
