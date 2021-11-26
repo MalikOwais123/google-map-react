e4d32faf1c9aa9fb074d3afb89234467bf9eff5e
Muhammad Owais <malikowaisjavaid@gmail.com>
2021-11-26 10:46:44 +0500

    Test Component logics

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Map from "./Component/Map/Map";
 import "./App.css";
+import Test from "./Component/Test/Test";
 
 function App() {
     const mapURL = `https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDCNkvhRgNcVWzX26GgylPyIJFB8bGxRj0`;
     return (
         <div className="App">
-            GOOGLE MAPS ADDED
-            GOOGLE MAPS ADDED
-            GOOGLE MAPS ADDED
-            GOOGLE MAPS ADDED
+            <Test/>
             {/* SOME PROPS MANDATORY TO RENDER MAP */}
             <Map
                 googleMapURL={mapURL}
---
+++ src/Component/Test/Test.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+
+const Test = () => {
+    const [players, setPlayers] = useState([]);
+    const [selectedTeam, setSelectedTeam] = useState("");
+    const teams = [
+        {
+            id: 1,
+            name: "Dumbs",
+            players: [
+                {
+                    id: 11,
+                    name: "Hamza",
+                    shirtNum: 11,
+                },
+                {
+                    id: 12,
+                    name: "Owais",
+                    shirtNum: 12,
+                },
+                {
+                    id: 13,
+                    name: "Hiba",
+                    shirtNum: 13,
+                },
+            ],
+        },
+        {
+            id: 12,
+            name: "Coders",
+            players: [
+                {
+                    id: 21,
+                    name: "Jaffar",
+                    shirtNum: 21,
+                },
+                {
+                    id: 22,
+                    name: "Shajil",
+                    shirtNum: 22,
+                },
+                {
+                    id: 23,
+                    name: "Ayub",
+                    shirtNum: 23,
+                },
+            ],
+        },
+        {
+            id: 13,
+            name: "Xorders",
+            players: [
+                {
+                    id: 31,
+                    name: "Ah",
+                    shirtNum: 31,
+                },
+                {
+                    id: 32,
+                    name: "Asss",
+                    shirtNum: 32,
+                },
+                {
+                    id: 33,
+                    name: "ASASASAS",
+                    shirtNum: 33,
+                },
+            ],
+        },
+    ];
+
+    const getTeamPlayers = (id) => {
+        const teamsPlayers = teams.find((d) => d.id === id);
+        const { players, name } = teamsPlayers;
+        setSelectedTeam(name);
+        setPlayers(players);
+    };
+
+    return (
+        <>
+            <div>
+                <h1>TEAMS</h1>
+                {teams.map((d) => (
+                    <>
+                        <div style={{ cursor: "pointer" }} onClick={() => getTeamPlayers(d.id)}>
+                            Team Name : {d.name}
+                        </div>
+                        <hr />
+                    </>
+                ))}
+
+                {selectedTeam && <h1>{selectedTeam} : Players</h1>}
+                {players.length > 0 &&
+                    players.map((d) => (
+                        <>
+                            <div style={{ cursor: "pointer" }} onClick={() => getTeamPlayers(d.id)}>
+                                Player Name : {d.name}
+                            </div>
+                            <hr />
+                        </>
+                    ))}
+            </div>
+        </>
+    );
+};
+
+export default Test;
